02c191e80bac27ec96c3423305ebd5eba47d4e49
xomaeda <b1ackparklo2l@gmail.com>
2025-12-30 16:42:52 +0900

    Update Commands.js

--- src/Commands.js
+++ src/Commands.js
@@ -1,1 +1,18 @@
+import { suspectLine, defendLine } from "./Dialogues.js";
+
+export const Commands = {
+  suspect(game, actor, target) {
+    target.trust[actor.id] = (target.trust[actor.id] ?? 50) - actor.stats.logic;
+    target.favor[actor.id] = (target.favor[actor.id] ?? 50) - actor.stats.acting;
+    actor.aggro += 5 - actor.stats.stealth * 0.1;
+    return suspectLine(actor, target);
+  },
+
+  defend(game, actor, target) {
+    target.trust[actor.id] = (target.trust[actor.id] ?? 50) + actor.stats.logic;
+    target.favor[actor.id] = (target.favor[actor.id] ?? 50) + actor.stats.acting;
+    actor.aggro += 3;
+    return defendLine(actor, target);
+  }
+};
 
