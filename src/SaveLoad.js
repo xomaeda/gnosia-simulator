d9f1f52ae8c9aaa1d7987d96a779486632f1e301
xomaeda <b1ackparklo2l@gmail.com>
2025-12-30 16:44:01 +0900

    Update SaveLoad.js

--- src/SaveLoad.js
+++ src/SaveLoad.js
@@ -1,1 +1,8 @@
+export function saveGame(game) {
+  localStorage.setItem("gnosiaSave", JSON.stringify(game));
+}
+
+export function loadGame() {
+  return JSON.parse(localStorage.getItem("gnosiaSave"));
+}
 
