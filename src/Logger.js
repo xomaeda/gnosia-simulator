3c3f61f1d1925623720e4ad8e68c2cffa2ce7084
xomaeda <b1ackparklo2l@gmail.com>
2025-12-30 17:08:11 +0900

    Update Logger.js

--- src/Logger.js
+++ src/Logger.js
@@ -1,1 +1,21 @@
+class Logger {
+  static header(text) {
+    console.log("\n====================");
+    console.log(text);
+    console.log("====================");
+  }
+
+  static log(text) {
+    console.log(text);
+  }
+
+  static revealRoles(characters) {
+    console.log("\n=== 역할 공개 ===");
+    characters.forEach(c => {
+      console.log(`${c.name} : ${c.role}`);
+    });
+  }
+}
+
+module.exports = Logger;
 
